35e0cc5e053a9bb77b2f10add2353b9834b1d437
ErikAguilar2418 <Trojan24#>
2018-12-07 13:31:33 -0800

    Messed with testsim bottom run

--- TestSim.py
+++ TestSim.py
@@ -146,19 +146,25 @@ class TestSim:
 
 def main():
     s = TestSim();
-    s.runTime(10);
+    s.runTime(30);
     s.loadTopo("long_line.topo");
     s.loadNoise("no_noise.txt");
     s.bootAll();
     s.addChannel(s.COMMAND_CHANNEL);
     s.addChannel(s.GENERAL_CHANNEL);
+    s.addChannel(s.NEIGHBOR_CHANNEL);
+    s.addChannel(s.FLOODING_CHANNEL);
+    s.runTime(30);
 
-    s.runTime(20);
-    s.ping(1, 2, "Hello, World");
-    s.runTime(10);
-    s.ping(1, 3, "Hi!");
-    s.runTime(20);
-    s.neighborDMP(2);
+    s.setAppServer();
+    s.runTime(30);
+    s.setAppClient(2, 49);
+
+    #s.ping(1, 2, "Hello, World");
+    #s.runTime(10);
+    #s.ping(1, 3, "Hi!");
+    s.runTime(200);
+    #s.neighborDMP(2);
 
 if __name__ == '__main__':
     main()
